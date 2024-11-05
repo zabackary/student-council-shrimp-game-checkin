46acc9791ced8fa6fea8f76ac4ba5878bca4f91b
zabackary <137591653+zabackary@users.noreply.github.com>
2024-11-05 22:03:40 +0900

    feat: add cfg features for cameras

--- src/backend/cameras.rs
+++ src/backend/cameras.rs
@@ -1,6 +1,8 @@
 use std::fmt::{Debug, Display};
 
+#[cfg(feature = "camera_gphoto2")]
 pub mod gphoto2;
+#[cfg(feature = "camera_nokhwa")]
 pub mod nokhwa;
 
 pub trait CameraBackend: Clone {
@@ -22,4 +24,14 @@ pub trait CameraBackendCamera: Send {
     fn capture_still_frame(&mut self) -> Result<image::RgbaImage, Self::Error>;
 }
 
+#[cfg(all(feature = "camera_nokhwa", feature = "camera_gphoto2"))]
+compile_error!(
+    "feature \"camera_nokhwa\" and feature \"camera_gphoto2\" cannot be enabled at the same time"
+);
+#[cfg(not(any(feature = "camera_nokhwa", feature = "camera_gphoto2")))]
+compile_error!("one of feature \"camera_nokhwa\" and feature \"camera_gphoto2\" should be enabled");
+
+#[cfg(feature = "camera_gphoto2")]
+pub type DefaultCameraBackend = gphoto2::GPhoto2Backend;
+#[cfg(feature = "camera_nokhwa")]
 pub type DefaultCameraBackend = nokhwa::NokhwaBackend;
--- src/frontend/main_app/animations.rs
+++ src/frontend/main_app/animations.rs
@@ -4,4 +4,7 @@ pub mod countdown_circle;
 pub mod ready;
 pub mod upsell_templates;
 
+#[cfg(feature = "fast_animations")]
+const LENGTH_DIVISOR: u64 = 10;
+#[cfg(not(feature = "fast_animations"))]
 const LENGTH_DIVISOR: u64 = 1;
